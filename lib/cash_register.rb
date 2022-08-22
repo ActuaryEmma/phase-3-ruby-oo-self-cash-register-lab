f927b6444ac6c1fca2f657f536537c6fcffb4992
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-08-22 08:44:52 +0300

    commit tree 8f08d7243f4ea074c84d5fc724957c857e177de4

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,12 @@
-
+class CashRegister
+     @total = 0
+    # attr_accessor :total, 
+    attr_accessor :total, :discount
+    def initialize(discount=0)
+        @discount = discount
+        self.total = 0
+    end
+    def add_item(title, price)
+        @total += price
+    end
+end
